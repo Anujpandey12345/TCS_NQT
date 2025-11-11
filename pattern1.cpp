18b09822b12bf877bb5032a0f0379be81be10387
Anuj Kumar <anujpandey61687@gmail.com>
2025-11-11 22:18:49 +0530

    pattern

--- pattern1.cpp
+++ pattern1.cpp
@@ -63,22 +63,66 @@
 //     return 0;
 // }
 
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+//     int n = 5;
+
+//     // Upper part
+//     for (int i = 1; i <= n; i++) {
+//         for (int j = 1; j < i; j++) {
+//             cout << "  ";
+//         }
+//         for (int j = i; j <= n; j++) {
+//             cout << char('A' + j - 1) << " ";
+//         }
+//         for (int j = i + 1; j <= n; j++) {
+//             cout << char('A' + j - 1) << " ";
+//         }
+//         cout << endl;
+//     }
+
+//     // Lower part
+//     for (int i = 1; i <= n; i++) {
+//         for (int j = i; j < n; j++) {
+//             cout << "  ";
+//         }
+//         for (int j = 1; j <= i; j++) {
+//             cout << char('A' + j - 1) << " ";
+//         }
+//         for (int j = 1; j < i; j++) {
+//             cout << char('A' + j - 1) << " ";
+//         }
+//         cout << endl;
+//     }
+
+//     return 0;
+// }
+
+
+
+
+
+
 #include <iostream>
+#include <iomanip>
 using namespace std;
 
 int main() {
     int n = 5;
+    int n1 = 1;
 
     // Upper part
     for (int i = 1; i <= n; i++) {
         for (int j = 1; j < i; j++) {
-            cout << "  ";
+            cout << "   ";
         }
         for (int j = i; j <= n; j++) {
-            cout << char('A' + j - 1) << " ";
+            cout << setw(3) << n1++;
         }
         for (int j = i + 1; j <= n; j++) {
-            cout << char('A' + j - 1) << " ";
+            cout << setw(3) << n1++;
         }
         cout << endl;
     }
@@ -89,10 +133,10 @@ int main() {
             cout << "  ";
         }
         for (int j = 1; j <= i; j++) {
-            cout << char('A' + j - 1) << " ";
+            cout << setw(3) << n1++;
         }
         for (int j = 1; j < i; j++) {
-            cout << char('A' + j - 1) << " ";
+            cout << setw(3) << n1++;
         }
         cout << endl;
     }
