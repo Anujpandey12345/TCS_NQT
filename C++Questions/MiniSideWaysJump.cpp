3d96d670fc602516f0bc7707c85836ef422db410
Anuj Kumar <anujpandey61687@gmail.com>
2025-12-08 23:08:07 +0530

    MiniSideWaysJump.cpp

---
+++ C++Questions/MiniSideWaysJump.cpp
@@ -0,0 +1,13 @@
+
+// 1824. Minimum Sideway Jumps --> Recursion Solution 
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    vector<int> obstacles = {0,2,1,0,3,0};
+    int n = obstacles.size();
+    for(int i  = 0; i < n; i++) {
+        cout << obstacles[i] << " ";
+    }
+    cout << endl;
+    return 0;
+}
