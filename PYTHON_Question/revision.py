1c5e657fe173daf98e478b96c71bdf9543c916b3
Anuj Kumar <anujpandey61687@gmail.com>
2025-12-18 13:20:38 +0530

    python revison

--- PYTHON_Question/revision.py
+++ PYTHON_Question/revision.py
@@ -1,8 +1,52 @@
 # This is Revision file
 
-n = int(input("Enter the Number :: "))
+# n = int(input("Enter the Number :: "))
 
-try:
-    print(10/n)
-except Exception as err:
-    print(f"You get an error as {err}")
+# try:
+#     print(10/n)
+# except Exception as err:
+#     print(f"You get an error as {err}")
+
+
+
+#Second Day
+
+# ram = "6"
+# n = int(ram)
+# print(n)
+# print(type(n))
+
+
+
+
+# name = input("Enter the Name :: ")
+
+# slicing = name[0 : 30: 2]
+# print(slicing.upper())
+# print(slicing.lower())
+# print(slicing.rstrip())
+
+
+#For loop questions
+# n = int(input("Enter the Number"))
+# for i in range(n):
+#     print("Hello World")
+
+
+
+# n = int(input("Enter the Number"))
+# for i in range(n):
+#     print(i)
+
+
+
+
+# n = int(input("Enter the Number :: "))
+# for i in range(n, 0, -1):
+#     print(i)
+
+c = 13
+a = 10
+b = c
+# print(a == b)
+print(a is b)
