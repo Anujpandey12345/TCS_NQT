cbb42b4b52852d2c5cb0740774b3ef054bc00ce1
Anuj Kumar <anujpandey61687@gmail.com>
2025-12-29 23:06:50 +0530

    Matplotlib

--- PYTHON_Question/Practice20.py
+++ PYTHON_Question/Practice20.py
@@ -10,15 +10,97 @@ import pandas as pd
 # df.to_csv("students.csv", index=False)
 # print("csv file created successfully")
 
-data = pd.read_csv("students.csv")
-print("Avarage marks of the students")
-for i in range(len(data)):
-    name = data['Name'][i]
-    marks = pd.Series([
-        data['Marks1'][i],
-        data['Marks2'][i],
-        data['Marks3'][i]
-    ])
-    avg = marks.mean()
+# data = pd.read_csv("students.csv")
+# print("Avarage marks of the students")
+# for i in range(len(data)):
+#     name = data['Name'][i]
+#     marks = pd.Series([
+#         data['Marks1'][i],
+#         data['Marks2'][i],
+#         data['Marks3'][i]
+#     ])
+#     avg = marks.mean()
 
-    print(name, ":", avg)
+#     print(name, ":", avg)
+
+
+import matplotlib.pyplot as plt
+
+# # Data
+# food = ["Meat", "Banana", "Avocado", "Sweet Potato", "Spinach", "Watermelon", "Beans"]
+# calories = [225, 130, 140, 120, 20, 20, 50]
+# potassium = [40, 50, 20, 30, 40, 32, 26]
+# fat = [8, 5, 3, 6, 1, 1.5, 2]
+
+# # Plot Calories
+# plt.figure()
+# plt.bar(food, calories)
+# plt.xlabel("Food Category")
+# plt.ylabel("Calories")
+# plt.title("Calories in Different Food Items")
+# plt.show()
+# # Plot Potassium
+# plt.figure()
+# plt.bar(food, potassium)
+# plt.xlabel("Food Category")
+# plt.ylabel("Potassium")
+# plt.title("Potassium in Different Food Items")
+# plt.show()
+# # Plot Fat
+# plt.figure()
+# plt.bar(food, fat)
+# plt.xlabel("Food Category")
+# plt.ylabel("Fat")
+# plt.title("Fat Content in Different Food Items")
+# plt.show()
+
+
+# # Data
+# Language = ["Python", "Java", "C++", "JavaScript", "Ruby"]
+# Popolarity = [30, 25, 20, 15, 10]
+# # Plot Popularity
+
+# plt.figure()
+# plt.bar(Language, Popolarity)
+# plt.xlabel("Language")
+# plt.ylabel("Popularity")
+# plt.title("Popularity is this")
+# plt.show()
+
+
+
+
+
+
+
+Food = ["Meat", "Banana", "Avacados", "SweetPotatos", "Spinch"]
+Calories = [250, 130, 140, 120, 20]
+Potassium = [40, 55, 20, 30, 40]
+Fat = [8, 5, 3, 6, 1]
+
+# Plot Calories
+plt.figure()
+plt.bar(Food, Calories)
+plt.xlabel("Food Category")
+plt.ylabel("Calories")
+plt.title("Calories is Diffrent food Items")
+plt.show()
+
+
+# Plot Potassium
+plt.figure()
+plt.bar(Food, Potassium)
+plt.xlabel("Food Category")
+plt.ylabel("Potassium")
+plt.title("Potassium is Diffrent food items")
+plt.show()
+
+
+# Plot Fat
+
+plt.figure()
+plt.bar(Food, Fat)
+plt.xlabel("Food Category")
+plt.ylabel("Fat")
+plt.title("Fat is Different food items")
+plt.show()
