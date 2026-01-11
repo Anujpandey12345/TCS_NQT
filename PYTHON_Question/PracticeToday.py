8e286ecd7034c71c92d31048289c53461de418a7
Anuj Kumar <anujpandey61687@gmail.com>
2026-01-11 19:31:20 +0530

    Implement Python practice exercises to strengthen core concepts

---
+++ PYTHON_Question/PracticeToday.py
@@ -0,0 +1,92 @@
+# DataType
+# 1. Numerical DataType
+# int  = 10
+# float = 10.9
+# complex = 35J
+print(type(int), type(float), type(complex))
+# 2. Sequence DataType
+str = "Hello Duniya"
+list = [90, 90]
+tuple = (32, 45, 87, 89)
+print(type(str), type(list), type(tuple))
+# 3. Mapping DataType
+dict = {1:23, 2:34, 3:65, 4:56}
+print(type(dict))
+# 4. Boolean Type
+# 5. Set Type
+
+
+
+
+"""Type Casting A Variable"""
+
+A = "10"
+n = int(A)
+print(type(n))
+
+
+
+"""Getting the Type of A variable"""
+A = 10
+print(type(A))
+
+
+"""Delete A variable"""
+
+# A = 10
+# del A
+# print(A)   # It gives you variable not define
+
+"""Reverse A String"""
+
+A = "Hello Duniya"
+rev = A[::-1]
+print(rev)
+B = "O"
+print(ord(B))
+C = 65
+print(chr(C))
+
+
+# """Q1. Accept two numbers and print the greatest between them."""
+# num1 = int(input("Enter the First Number :: "))
+# num2 = int(input("Enter the Second Number :: "))
+# if num1 > num2:
+#     print(f"{num1} (NUM1) is greater than (NUM2) {num2}")
+# else:
+#     print(f"{num2} (NUM2) is greater than (NUM1) {num1}")
+
+
+
+
+
+
+
+"""Function in Python"""
+
+
+# def rem(a, b):
+#     return a + b
+# print(rem(10, 29))
+
+
+"""types of Functions"""
+# 1. Built in function
+# 2. User define function
+# 3. Anonymous (lambda) function - > these are nameless functions which is made by using keywords
+
+
+
+
+
+"""Array's in python"""
+# - > Array's store the data in contnous locations with same data type but in python we do not have array but by using the numpy we can access the array
+
+# import numpy as np 
+# data = [12, 43, 56, 78, 89]
+# arr = np.array(data)
+# print(f"type of Array is {type(arr)} and the value is {arr}")
+
+
+
+
