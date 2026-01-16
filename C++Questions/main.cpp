660aec9baee9de30a5d2e82de7302a3858eefb3e
Anuj Kumar <anujpandey61687@gmail.com>
2026-01-16 20:02:58 +0530

    Count Occurrences in a Linked List

--- C++Questions/main.cpp
+++ C++Questions/main.cpp
@@ -108,3 +108,55 @@ head.next.next.prev = head.next;
 
 head = delHead(head);
 printList(head);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Javascript program to count occurrences in
+// a linked list by recursion
+class Node {
+    constructor(data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+// Recursive function to count occurrences of a 
+// value in the linked list
+function count(head, key) {
+    if (head === null) {
+        return 0;
+    }
+    
+    let ans = count(head.next, key);
+    
+    if (head.data === key) {
+        ans++;
+    }
+    
+    return ans;
+}
+
+// Hard coded linked list:
+// 1 -> 2 -> 1 -> 2 -> 1
+let head = new Node(1);
+head.next = new Node(2);
+head.next.next = new Node(1);
+head.next.next.next = new Node(2);
+head.next.next.next.next = new Node(1);
+
+let key = 1;
+
+console.log( count(head, key));
